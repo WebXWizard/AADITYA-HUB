afd33de8fe24667cfd555a6e91bb72a7cf3558de
Shahe Alam <alamshahe9044@gmail.com>
2025-06-20 10:22:18 +0530

    Cors Error Fixed

--- Backend/index.js
+++ Backend/index.js
@@ -15,10 +15,11 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://aaditya-hub.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
-
 app.use("/user", UserRouter);
 app.use("/contact", ContactRouter);
 app.use("/admin", AdminRouter);
